a586ed6b2fd2e8be571bd7fd56edc3b7f1cbe40f
pawel.mrowczynski <pawel.mrowczynski@outlook.com>
2018-12-23 21:25:52 +0100

    Project skeleton with xUnit tests that VS can see...

--- SharpPlayground/Tests/PLinqTests/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace PLinqTests
-{
-    public class Class1
-    {
-    }
-}
---
+++ SharpPlayground/Tests/PLinqTests/WhereTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace PLinqTests
+{
+    public class WhereTests
+    {
+        [Fact]
+        public void NullSourceThrouwsNullArgumentException()
+        {
+            IEnumerable<int> source = null;
+            Assert.Throws<ArgumentNullException>(() => source.Where(x => x == 1));
+        }        
+        
+        [Fact]
+        public void NullPredicateThrowsNullArgumentException()
+        {
+            int[] source = {1, 2, 3, 4, 5, 6, 7};
+            Func<int, bool> predicate = null;
+            Assert.Throws<ArgumentNullException>(() => source.Where(predicate));
+        }       
+        
+        [Fact]
+        public void WithIndexNullSourceThrowsNullArgumentException()
+        {
+            IEnumerable<int> source = null;
+            Assert.Throws<ArgumentNullException>(() => source.Where((x, index) => x == 1));
+        }        
+        
+        [Fact]
+        public void WithIndexNullPredicateThrowsNullArgumentException()
+        {
+            int[] source = {1, 2, 3, 4, 5, 6, 7};
+            Func<int, int, bool> predicate = null;
+            Assert.Throws<ArgumentNullException>(() => source.Where(predicate));
+        }
+
+        
+        
+    }
+}
